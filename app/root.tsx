548270f30041cfad2504d64631cc0cecaace7f7d
Kerry Mahne <kerry.mahne@gmail.com>
2022-03-17 20:36:12 +0100

    some scaffolding, couldnt header until the end

--- app/root.tsx
+++ app/root.tsx
@@ -5,18 +5,34 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+} from 'remix';
+import type { MetaFunction } from 'remix';
+import { createGlobalStyle } from 'styled-components';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'Mixing it up',
+  viewport: 'width=device-width,initial-scale=1',
 });
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    width: 100vw;
+    height: 100vh;
+  }
+  body {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+  }
+`;
 
 export default function App() {
   return (
     <html lang="en">
+      <GlobalStyle />
       <head>
         <Meta />
         <Links />
---
+++ app/theme/colors.ts
@@ -0,0 +1,4 @@
+export const FONT_1: string = '#563421';
+export const FONT_2: string = '#72442b';
+export const BACKGROUND_1: string = '#9a835e';
+export const BACKGROUND_2: string = '#cca57d';
